13be6ac6032e2977f9eef243a5f0fbb4d19dd119
phuriphak3 <phuriphak_cha@cmu.ac.th>
2025-02-11 10:08:48 +0700

    lab17_2.cpp completed

--- lab17_2.cpp
+++ lab17_2.cpp
@@ -4,16 +4,6 @@ using namespace std;
 void myString( char *&p,int N){
       p = new char[N+1];    
 	for(int i = 0; i < N;i++) p[i] = 'A'+i;
-		p[N+1]  = 0;
+		p[N]  = 0;
 }
 
-int main(){
-	char *p;
-	int N;	
-	cout << "N = ";
-	cin >> N;		
-	myString(p,N);
-	cout << p;	
-	delete [] p;
-	return 0;
-}
